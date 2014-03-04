233de4c254ae3f02f0a19438ef9bae44898b109d
James Halliday <mail@substack.net>
2014-03-03 23:37:29 -0800

    roller works

---
+++ example/sink.js
@@ -0,0 +1,7 @@
+var sinker = require('../');
+var minimist = require('minimist');
+var argv = minimist(process.argv.slice(2));
+
+var dir = argv._[0];
+var sink = sinker(dir);
+process.stdin.pipe(sink).pipe(process.stdout);
