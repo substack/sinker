940b8f29185af712ed8e0bb84376005eaf1472f6
James Halliday <mail@substack.net>
2014-05-05 16:47:19 -0700

    use typewise for sorting

--- test/ops.js
+++ test/ops.js
@@ -3,8 +3,9 @@ var path = require('path');
 var net = require('net');
 var test = require('tape');
 var fs = require('fs');
+var typewise = require('typewise');
 
-// bump the ctime on two.txt:
+// bump the ctime on two.txt so that b's version wins:
 var twofile = path.join(__dirname, 'ops/b/two.txt');
 fs.writeFileSync(twofile, fs.readFileSync(twofile));
 
@@ -15,15 +16,15 @@ test('verify operation lists', function (t) {
     var b = sinker(path.join(__dirname, 'ops/b'));
     
     a.on('ops', function (ops) {
-        t.deepEqual(ops, [
-            [ 'MOVE', 'here.txt', 'there.txt' ],
+        t.deepEqual(ops.sort(typewise.compare), [
             [ 'FETCH', 'three.txt' ],
-            [ 'FETCH', 'two.txt' ]
+            [ 'FETCH', 'two.txt' ],
+            [ 'MOVE', 'here.txt', 'there.txt' ]
         ]);
     });
     
     b.on('ops', function (ops) {
-        t.deepEqual(ops, [
+        t.deepEqual(ops.sort(typewise.compare), [
             [ 'FETCH', 'foo/beep.txt' ],
             [ 'FETCH', 'one.txt' ]
         ]);
