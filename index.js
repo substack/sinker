ee3ff9d1cc80c6528233a13129f695318b6930b7
James Halliday <mail@substack.net>
2014-05-05 15:58:11 -0700

    proxy through reads and writes to the plex instance directly

--- index.js
+++ index.js
@@ -6,25 +6,28 @@ var walkDir = require('findit');
 var inherits = require('inherits');
 var through = require('through2');
 var concat = require('concat-stream');
-var Duplex = require('readable-stream').Duplex;
 var split = require('split');
+var mdm = require('mux-demux');
+
+var Duplex = require('readable-stream').Duplex;
+var Readable = require('readable-stream').Readable;
 
 var version = require('./package.json').protocolVersion;
 
-var mdm = require('mux-demux');
-
-module.exports = function (dir) {
-    return new Sinker(dir).plex;
-};
+module.exports = Sinker;
+inherits(Sinker, Duplex);
 
 function Sinker (dir) {
     var self = this;
     if (!(this instanceof Sinker)) return new Sinker(dir);
+    Duplex.call(this);
     
     var plex = mdm(function (stream) {
         stream.pipe(self.readCommands());
     });
     this.plex = plex;
+    this.plexReadable = Readable().wrap(plex);
+    
     this.dir = dir;
     this.files = { local: {}, remote: {} };
     this.hashes = { local: {}, remote: {} };
@@ -38,6 +41,25 @@ function Sinker (dir) {
     this._prelude();
 }
 
+Sinker.prototype._read = function (n) {
+    var self = this;
+    var buf, times = 0;
+    while ((buf = self.plexReadable.read()) !== null) {
+        self.push(buf);
+        times ++;
+    }
+    if (times === 0) {
+        self.plexReadable.once('readable', function () {
+            self._read(n);
+        });
+    }
+};
+
+Sinker.prototype._write = function (buf, enc, next) {
+    this.plex.write(buf);
+    next();
+};
+
 Sinker.prototype._prelude = function () {
     var self = this;
     var dir = path.resolve(this.dir);
@@ -48,13 +70,12 @@ Sinker.prototype._prelude = function () {
         pending ++;
         var rel = path.relative(dir, path.resolve(dir, file));
         hashFile(file, function (err, hash) {
-            var row = {
+            self.files.local[rel] = {
                 hash: hash,
                 time: stat.mtime.valueOf()
             };
-            self.files.local[rel] = row;
             if (!self.hashes.local[hash]) self.hashes.local[hash] = [];
-            self.hashes.local[hash].push(row);
+            self.hashes.local[hash].push(rel);
             
             self.send([ 'HASH', rel, hash, stat.mtime.valueOf() ]);
             if (-- pending === 0) done();
@@ -74,10 +95,17 @@ Sinker.prototype._sync = function () {
     this.mode = 'SYNC';
     
     var ops = [];
-    Object.keys(this.files.remote).forEach(function (key) {
-        // ...
+    Object.keys(this.files.local).forEach(function (key) {
+        var lf = self.files.local[key];
+        var rf = self.files.remote[key];
+        if (!rf && self.hashes[lf.hash]) {
+            ops.push([ 'COPY', key, self.hashes[lf.hash] ]);
+        }
+        else if (!rf) {
+            ops.push([ 'FETCH', lf.hash, key ]);
+        }
     });
-    console.log(self.hashes);
+    console.log(ops);
 };
 
 Sinker.prototype.execute = function (cmd) {
@@ -91,13 +119,12 @@ Sinker.prototype.execute = function (cmd) {
     }
     else if (cmd[0] === 'HASH') {
         var file = cmd[1], hash = cmd[2];
-        var row = {
+        this.files.remote[file] = {
             hash: hash,
             time: cmd[3] + this._clockSkew
         };
-        this.files.remote[file] = row;
         if (!this.hashes.remote[hash]) this.hashes.remote[hash] = [];
-        this.hashes.remote[hash].push(row);
+        this.hashes.remote[hash].push(file);
     }
 };
 
